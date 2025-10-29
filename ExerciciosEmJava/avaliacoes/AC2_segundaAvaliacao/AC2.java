9c6b3142eb793cd4e0b0cff5dd93cc4c49f13e8d
Matheus Marcelo <matheuscrudi095@gmail.com>
2025-10-29 01:17:34 -0300

    adição e inicio do simulado ac2

--- ExerciciosEmJava/avaliacoes/AC2_segundaAvaliacao/AC2.java
+++
@@ -1,8 +0,0 @@
-package avaliacoes.AC2_segundaAvaliacao;
-
-
-public class AC2 {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ ExerciciosEmJava/avaliacoes/AC2_segundaAvaliacao/simulado/simuladoAC2.java
@@ -0,0 +1,6 @@
+package avaliacoes.AC2_segundaAvaliacao.simulado;
+
+public class simuladoAC2 {
+    public static void main(String[] args){
+    }
+}
