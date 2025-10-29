b5a9aa1bfd5ea2cefac52c7fac902aa7b3ffb30f
Matheus Marcelo <matheuscrudi095@gmail.com>
2025-10-29 18:54:42 -0300

    Inicio do simulado AC2

--- ExerciciosEmJava/avaliacoes/AC2_segundaAvaliacao/simulado/simuladoAC2.java
+++ ExerciciosEmJava/avaliacoes/AC2_segundaAvaliacao/simulado/simuladoAC2.java
@@ -1,6 +1,28 @@
 package avaliacoes.AC2_segundaAvaliacao.simulado;
+import java.util.Scanner;
 
 public class simuladoAC2 {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int estoqueMinimo, produtosCadastrados;
+        
+        System.out.println("Qual é a quantidade minima de estoque que um produto pode ter?");
+        estoqueMinimo = sc.nextInt();
+        if (estoqueMinimo <= 0) System.out.println("ERRO!! O numero deve ser maior do que zero(0)");
+        
+        System.out.print("Qual é o total de produtos que voce vai cadastrar?\nTotal: ");
+        produtosCadastrados = sc.nextInt();
+        if (produtosCadastrados <= 0) System.out.println("ERRO!! O numero deve ser maior do que zero(0)");
+        
+        String[] array1 = new String[produtosCadastrados];
+
+        for (int i = 0; i < produtosCadastrados; i++){
+            String nome;
+            System.out.println("Digite o nome do produto " + (i+1));
+            nome = sc.nextLine();
+            array1[i] = nome;
+        }
+
+        System.out.println(array1);
     }
 }
