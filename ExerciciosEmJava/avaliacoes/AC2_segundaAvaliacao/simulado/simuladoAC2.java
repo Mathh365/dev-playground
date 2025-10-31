e3f014f3ba09742dc66be1f14b820b85cf5adbb7
Matheus Marcelo <matheuscrudi095@gmail.com>
2025-10-31 00:35:50 -0300

    Update simuladoAC2.java

--- ExerciciosEmJava/avaliacoes/AC2_segundaAvaliacao/simulado/simuladoAC2.java
+++ ExerciciosEmJava/avaliacoes/AC2_segundaAvaliacao/simulado/simuladoAC2.java
@@ -4,27 +4,50 @@ import java.util.Scanner;
 public class simuladoAC2 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int estoqueMinimo, produtosCadastrados;
+        int estoqueMinimo = 0, produtosACadastrar = 0, soma = 0;
         
         System.out.println("Qual é a quantidade minima de estoque que um produto pode ter?");
         estoqueMinimo = sc.nextInt();
         if (estoqueMinimo <= 0) System.out.println("ERRO!! O numero deve ser maior do que zero(0)");
         
         System.out.print("Qual é o total de produtos que voce vai cadastrar?\nTotal: ");
-        produtosCadastrados = sc.nextInt();
-        if (produtosCadastrados <= 0) System.out.println("ERRO!! O numero deve ser maior do que zero(0)");
+        produtosACadastrar = sc.nextInt();
+        if (produtosACadastrar <= 0) System.out.println("ERRO!! O numero deve ser maior do que zero(0)");
         sc.nextLine();
         
-        String[] nomesProdutos = new String[produtosCadastrados];
-        int[] estoqueProdutos = new int[produtosCadastrados];
+        String[] nomeDosProdutos = new String[produtosACadastrar];
+        int[] quantidade = new int[produtosACadastrar];
+        String[] status = new String[produtosACadastrar];
 
-        for (int i = 0; i < produtosCadastrados; i++){
-            System.out.print("Digite o nome do produto " + (i + 1) + "\n:");
-            nomesProdutos[i] = sc.nextLine();
+        for (int i = 0; i < produtosACadastrar; i++){
+            System.out.print("\nDigite o nome do produto " + (i + 1) + "\nProduto:");
+            nomeDosProdutos[i] = sc.nextLine();
+            System.out.print("Quantos voce tem em estoque?\nEstoque:");
+            quantidade[i] = sc.nextInt();
+            sc.nextLine();
+
+            if (quantidade[i] >= estoqueMinimo) status[i] = "Adequado";
+            else if (quantidade[i] >= (estoqueMinimo / 2)) status[i] = "Baixo";
+            else if (quantidade[i] < (estoqueMinimo / 2)) status[i] = "Critico";
+
+            soma += quantidade[i];
+            
+            System.out.printf("""
+
+                Produto: %s
+                Quantidade: %d
+                Status: %s\n""", nomeDosProdutos[i], quantidade[i], status[i]);
         }
 
-        // for (int i = 0; i < nomesProdutos.length; i++){
-        //     System.out.println(nomesProdutos[i]);
-        // }
+        for (int i = 0; i < nomeDosProdutos.length; i++){
+            System.out.printf("""
+            
+            Segue um relatorio de todos os produtos em estoque:
+            -Nome do produto             : %s
+            -Quantidade em estoque       : %d
+            -Status                      : %s\n""",nomeDosProdutos[i], quantidade[i], status[i]);
+        }
+        System.out.println("Total de produtos em estoque: " + soma);
     }
+    
 }
