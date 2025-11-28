1c412c8186ac290dfc32813df82c1cdd06d85fca
Matheus Marcelo <matheuscrudi095@gmail.com>
2025-11-28 17:26:14 -0300

    decriptografador de status python

--- ExerciciosEmPython/Decodificação de status/main.py
+++ ExerciciosEmPython/Decodificação de status/main.py
@@ -13,23 +13,24 @@ import funcoes.Conversor as f
 # bits: 4 a 0 Temperatura Temperatura em graus (0 a 31)
 
 
-# temperatura = str()
-# nivelDaBateria = str()
-# tipoDeErro = str()
-# codigoDoModulo = str()
+temperatura = str()
+nivelDaBateria = str()
+tipoDeErro = str()
+codigoDoModulo = str()
 
 binario = f.HexToBin("B29E")
-for i, item in enumerate(binario):
-#     if (13 <= i <= 15): codigoDoModulo += item
-#     if (10 <= i <= 12): tipoDeErro += item
-#     if (5 <= i <= 9): nivelDaBateria += item
-#     if (0 <= i <= 4): temperatura += item
+for i, item in enumerate(binario[::-1]):
+    if (0 <= i <= 4): temperatura += item
+    if (13 <= i <= 15): codigoDoModulo += item
+    if (10 <= i <= 12): tipoDeErro += item
+    if (5 <= i <= 9): nivelDaBateria += item
     
 
-# print(f"""
-#       Codigo do modulo que enviou este status: {(f.BinToDecimal(codigoDoModulo))}
-#       Tipo do erro: {f.BinToDecimal(tipoDeErro)}
-#       Nivel da bateria: {f.BinToDecimal(nivelDaBateria)}
-#       Temperatura: {f.BinToDecimal(temperatura)}
-#       """)
+print(f"""
+      Codigo do modulo que enviou este status: {(f.BinToDecimal(codigoDoModulo[::-1]))}
+      Tipo do erro: {f.BinToDecimal(tipoDeErro[::-1])}
+      Nivel da bateria: {f.BinToDecimal(nivelDaBateria[::-1])}
+      Temperatura: {f.BinToDecimal(temperatura[::-1])}
+      """)
+
 
