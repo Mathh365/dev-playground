8d6ff7253bd8049072e14a01c1c9bbbd655a3406
Matheus Marcelo <matheuscrudi095@gmail.com>
2025-11-28 00:14:05 -0300

    updt

--- ExerciciosEmCSharp/CRUDs/CRUD_basico/obj/Debug/net10.0/CRUD_basico.AssemblyInfo.cs
+++ ExerciciosEmCSharp/CRUDs/CRUD_basico/obj/Debug/net10.0/CRUD_basico.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD_basico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+142f9242dd544e15cdf0faf18297b2cfbec2b417")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31ac3c6010f89ef12a3484d9ed46a8b33b4371a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD_basico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD_basico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
