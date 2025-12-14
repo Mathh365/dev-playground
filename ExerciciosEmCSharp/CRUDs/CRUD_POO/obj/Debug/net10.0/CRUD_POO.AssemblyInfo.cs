9585c805f6963be67b6f906449efce4cdd6a27fd
Matheus Marcelo <matheuscrudi095@gmail.com>
2025-12-13 23:03:49 -0300

    ,

--- ExerciciosEmCSharp/CRUDs/CRUD_POO/obj/Debug/net10.0/CRUD_POO.AssemblyInfo.cs
+++ ExerciciosEmCSharp/CRUDs/CRUD_POO/obj/Debug/net10.0/CRUD_POO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD_POO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a7eaa28ffed22f1fe58cf0520a2d656eecf4f88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d952cb0b3749a59b28888040dabcfb843772404d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD_POO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD_POO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
