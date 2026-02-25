531f1fd9ef74210a6284b2c5c4ac749e0930dcf7
Matheus Marcelo <matheuscrudi095@gmail.com>
2026-02-25 00:28:24 -0300

    .

---
+++ Java/SegundoSemestre/Lista1/Ex001.java
@@ -0,0 +1,32 @@
+//Usando a entrada de dados recebe quatro valores decimais, respectivos as notas de cada bimestre.
+//Depois de coletadas a quatro notas, realizar operação de soma e médiadas notas, mostrando em tela.
+
+import java.util.Scanner;
+
+public class Ex001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double notas[] = new double[4];
+        int bimestre = 1;
+        for (int i = 0; i < notas.length; i++) {
+
+            do {
+                System.out.println("Digite a nota do bimestre " + bimestre);
+                notas[i] = sc.nextInt();
+                
+                if ((notas[i] > 10) || (notas[i] < 0)) System.out.println("Valor invalido, digite um valor entre 0 e 10");
+            } while (notas[i] < 0 || notas[i] > 10);
+
+            bimestre++;
+            System.out.println(notas[i]);
+        }
+        double soma = 0;
+
+        for(double nota : notas){
+            soma += nota;
+        }
+        double media = soma / notas.length;
+        System.out.printf("A sua media é " + media);
+    }
+}
---
+++ Java/SegundoSemestre/Lista1/Ex002.java
@@ -0,0 +1,19 @@
+//Usando as estruturas de Decisão, construa um programa que tenha o seguinte comportamento:
+//Ao digitar o número 1 o programa irá mostrar “Domingo”. Faça isso para todos os dias da semana.
+//Tenha em mente que terá o seguinte comportamento
+//        1 -> Domingo
+//        2 -> Segunda
+//        3 -> Terça
+//        4 -> Quarta
+//        5 -> Quinta
+//        6 -> Sexta
+//        7 -> Sábado
+//        Se o usuário digitar um dia inexistente mostra uma mensagem “Dia Inválido”.
+
+import java.util.Scanner;
+
+public class Ex002 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
