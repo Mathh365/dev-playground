7fd6dd99ae3972a82231b9847867736e57366bcd
Matheus Marcelo <matheuscrudi095@gmail.com>
2026-02-25 12:44:43 -0300

    ex007

--- Java/SegundoSemestre/Lista1/Ex008.java
+++ Java/SegundoSemestre/Lista1/Ex008.java
@@ -5,7 +5,21 @@ import java.util.Scanner;
 public class Ex008 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int num1, num2;
+
+        System.out.println("Digite um numero inteiro");
+        num1 = (int)sc.nextInt();
+        int menor = num1;
+        int maior = num1;
         
-        
+        System.out.println("Digite outro numero inteiro");
+        num2 = (int)sc.nextInt();
+
+        if (num2 < menor) menor = num2;
+        if (num2 > maior) maior = num2;
+
+        for (int i = menor; i <= maior; i++) {
+            if (i % 2 == 0) System.out.println(i);
+        }
     }
 }
