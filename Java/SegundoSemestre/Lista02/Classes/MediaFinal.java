eeeb9efe6fe616011c448bb041310f72f3e09370
Matheus Marcelo <matheuscrudi095@gmail.com>
2026-03-09 17:17:00 -0300

    lista 2 finalizada

--- Java/SegundoSemestre/Lista02/Classes/MediaFinal.java
+++ Java/SegundoSemestre/Lista02/Classes/MediaFinal.java
@@ -1,27 +1,55 @@
 package SegundoSemestre.Lista02.Classes;
 
+import java.lang.classfile.instruction.DiscontinuedInstruction.RetInstruction;
+
 public class MediaFinal {
 
-    public double AC1(double ac1l1, double ac1l2){
-        if (NotaValida(ac1l1) && NotaValida(ac1l2)){
-            return ((ac1l1 * 0.5) + (ac1l2 * 0.5));
-        }
-        else{
-            System.out.println("Notas invalidas");
-            return -1;
-        }
+    private double AC1(double l1, double l2){
+        if (IsValid(l1) && IsValid(l2)) return ((l1 * 0.5) + (l2 * 0.5));
+        System.out.println("Notas invalidas, digite uma nota entre 0 e 10");
+        return -1;
     }
-    public void AC2(){
+    
+    private double AC2(double l3, double l4, double l5, double l6, double l7){
+        if (!IsValid(l3)) {System.out.println("Uma ou mais notas sao invalidas, por favor digite uma nota entre 0 e 10"); return -1;}
+        if (!IsValid(l4)) {System.out.println("Uma ou mais notas sao invalidas, por favor digite uma nota entre 0 e 10"); return -1;}
+        if (!IsValid(l5)) {System.out.println("Uma ou mais notas sao invalidas, por favor digite uma nota entre 0 e 10"); return -1;}
+        if (!IsValid(l6)) {System.out.println("Uma ou mais notas sao invalidas, por favor digite uma nota entre 0 e 10"); return -1;}
+        if (!IsValid(l7)) {System.out.println("Uma ou mais notas sao invalidas, por favor digite uma nota entre 0 e 10"); return -1;}
 
-    }
-    public void AG(){
-
-    }
-    public void AF(){
-
+        return (l3 + l4 + l5 + l6 + l7) / 5;
     }
 
-    public boolean NotaValida(double nota) {
-        return nota >= 0 && nota <= 10;
+    private double AF(double p1, double p2){
+        if (IsValid(p1) && IsValid(p2)) return (p1 + p2) / 2;
+        System.out.println("Nota invalida, digite uma nota entre 0 e 10");
+        return -1;
     }
+
+    private boolean IsValid(double nota) {
+        return (nota >= 0) && (nota <= 10);
+    }
+
+    private double Media(double ac1, double ac2, double ag, double af){
+        return (ac1 * 0.15) + (ac2 * 0.30) + (ag * 0.1) + (af * 0.45);
+    }
+
+    public double ExecutorDaPipeline(double l1, double l2, double l3, double l4, double l5, double l6, double l7, double ag, double afParte1, double afParte2) {
+
+    double ac1 = AC1(l1, l2);
+    if (ac1 == -1) return -1;
+
+    double ac2 = AC2(l3, l4, l5, l6, l7);
+    if (ac2 == -1) return -1;
+
+    double af = AF(afParte1, afParte2);
+    if (af == -1) return -1;
+
+    if (!IsValid(ag)) {
+        System.out.println("Nota AG inválida");
+        return -1;
+    }
+
+    return Media(ac1, ac2, ag, af);
+}
 }
--- Java/SegundoSemestre/Lista02/Ex009.java
+++ Java/SegundoSemestre/Lista02/Ex009.java
@@ -2,13 +2,16 @@
 
 package SegundoSemestre.Lista02;
 
+import java.util.Scanner;
+
 import SegundoSemestre.Lista02.Classes.MediaFinal;
 
 public class Ex009 {
     public static void main(String[] args) {
         MediaFinal m = new MediaFinal();
-
-        System.out.println(m.AC1(10, 5));
+        
+        double nota = m.ExecutorDaPipeline(10, 6, 9, 8, 9, 6, 4, 7, 7, 8);
+        System.out.printf("%.2f\n", nota);
     }
 }
  
